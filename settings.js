cf25dae28fe18e320f4d428c6116fda1d5ca65e7
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2023-12-03 18:52:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,24 +21,24 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.owner = ['2348143692706'] //ur owner number
+global.ownernomer = "2348143692706" //ur owner number2
+global.ownername = "💕𝐅𝐫𝐞𝐝𝐝𝐲 𝐏𝐡𝐨𝐞𝐧𝐢𝐱 𝐌𝐢𝐥𝐥𝐬💕" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.socialm = "GitHub: Anonphoenix007" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.ownernumber = '2348143692706'
+global.ownername = '💕𝐅𝐫𝐞𝐝𝐝𝐲 𝐏𝐡𝐨𝐞𝐧𝐢𝐱 𝐌𝐢𝐥𝐥𝐬💕'
+global.ownerNumber = ["2348143692706@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.botscript = 'https://github.com/Anonphoenix007/CheemsBot-MD8' //script link
 global.packname = "Sticker By"
 global.author = "🦄드림 가이 Xeon\n\n"
 global.creator = "916909137213@s.whatsapp.net"
