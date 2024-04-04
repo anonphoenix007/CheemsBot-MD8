b0475391eee1c1032fd379c65baa626a72da35d2
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-04-04 09:07:40 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 
 //aumto presence update
 global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.autoblockmorroco = false //auto block 212 (true to on, false to off)
 global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
 global.antispam = false //auto kick spammer (true to on, false to off)
@@ -21,28 +21,28 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['2349072958046'] //ur owner number
-global.ownernomer = "2349072958046" //ur owner number2
-global.ownername = "💕✝️💟ᖴᖇEᗪEᖇIᑕK💟 ✝️💕" //ur owner name
+global.owner = ['2349031616504'] //ur owner number
+global.ownernomer = "2349031616504" //ur owner number2
+global.ownername = "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: Anonphoenix007" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = "Cheems Bot MD V8"
-global.ownernumber = '2349072958046'
-global.ownername = '💕✝️💟ᖴᖇEᗪEᖇIᑕK💟 ✝️𝐥𝐬💕'
-global.ownerNumber = ["2349072958046@s.whatsapp.net"]
+global.ownernumber = '2349031616504'
+global.ownername = 'Tᴀɪʀᴀ Mᴀᴋɪɴᴏ'
+global.ownerNumber = ["2349031616504@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.wagc = "https://chat.whatsapp.com/1pKH"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/Anonphoenix007/CheemsBot-MD8' //script link
 global.packname = "Sticker By"
 global.author = "🦄『••  ᖴᖇEᗪEᖇIᑕK  ••』 \n\n"
-global.creator = "2349072958046s.whatsapp.net"
-global.prefa = ['-']
+global.creator = "2349031616504.whatsapp.net"
+global.prefa = ['.']
 global.sessionName = 'session'
 global.hituet = 0
 
